3c7dc157eb74e42a1b2cd8fe08fa92beec00ade0
yshashanky <812shanky@gmail.com>
2022-05-21 15:58:22 +0530

    updaing locust file

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Starting with locust:
 - main file:  locustfile.py
 - to run with the web UI, run with following command: locust
-- for direct command line usage use: locust --headless --users 10 --spawn-rate 1 -H http://your-server.com
+- for direct command line usage use: locust --headless --users 10 --spawn-rate 1 -H https://oceanready-personalinfo-ui.prod.ocean.com/ (http://your-server.com)
  
--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,19 @@
-from locust import HttpUser, task
+import time
+from locust import HttpUser, task, between
 
-class HelloWorldUser(HttpUser):
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+
     @task
     def hello_world(self):
-        self.client.get("/hello")
+        self.client.get("/")
         self.client.get("/world")
+
+    @task(3)
+    def view_items(self):
+        for item_id in range(10):
+            self.client.get(f"/item?id={item_id}", name="/item")
+            time.sleep(1)
+
+    def on_start(self):
+        self.client.post("/", json={"username":"", "password":""})
