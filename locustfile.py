5bae557d20e47027a52a1f8f6285ac52351681be
yshashanky <812shanky@gmail.com>
2022-05-25 01:14:23 +0530

    update

--- locustfile.py
+++ locustfile.py
@@ -3,22 +3,36 @@ from locust import HttpUser, task, between, User, constant
 
 #Sample HttpUser Class:
 
-class QuickstartUser(HttpUser):
-    wait_time = between(1, 5)
+class MyReqRes(HttpUser):
+
+    host = "https://regres.in"
+    #wait_time = between(1, 5)
+    wait_time = constant(1)
 
     @task
-    def hello_world(self):
-        self.client.get("/")
-        self.client.get("/world")
+    def get_users(self):
+        res = self.client.get("/api/users?page=2")
+        print(res.text)
+        print(res.status_code)
+        print(res.headers)
 
-    @task(3)
-    def view_items(self):
-        for item_id in range(10):
-            self.client.get(f"/item?id={item_id}", name="/item")
-            time.sleep(1)
+    @task
+    def create_users(self):
+        res = self.client.post("/api/users", data=''' 
+            {"name":"morpheus","job":"leader"}
+        ''')
+        print(res.text)
+        print(res.status_code)
+        print(res.headers)
 
-    def on_start(self):
-        self.client.post("/", json={"username":"", "password":""}) 
+    # @task
+    # def view_items(self):
+    #     for item_id in range(10):
+    #         self.client.get(f"/item?id={item_id}", name="/item")
+    #         time.sleep(1)
+
+    # def on_start(self):
+    #     self.client.post("/", json={"username":"", "password":""}) 
         
 
 #Sample User Class:
