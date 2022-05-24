9ad1f78812ce1f77be23989560fa386bf9653581
yshashanky <812shanky@gmail.com>
2022-05-25 00:40:28 +0530

    update

--- locustfile.py
+++ locustfile.py
@@ -1,46 +1,48 @@
 import time
 from locust import HttpUser, task, between, User, constant
 
-#Sample code 1:
-# class QuickstartUser(HttpUser):
-#     wait_time = between(1, 5)
+#Sample HttpUser Class:
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/")
+        self.client.get("/world")
+
+    @task(3)
+    def view_items(self):
+        for item_id in range(10):
+            self.client.get(f"/item?id={item_id}", name="/item")
+            time.sleep(1)
+
+    def on_start(self):
+        self.client.post("/", json={"username":"", "password":""}) 
+        
+
+#Sample User Class:
+
+# class MyFirstTest(User):
+#     weight = 2
+#     wait_time = constant(1)
 
 #     @task
-#     def hello_world(self):
-#         self.client.get("/")
-#         self.client.get("/world")
+#     def launch(self):
+#         print("Launching the URL")
 
-#     @task(3)
-#     def view_items(self):
-#         for item_id in range(10):
-#             self.client.get(f"/item?id={item_id}", name="/item")
-#             time.sleep(1)
+#     @task
+#     def search(self):
+#         print("Searching")
 
-#     def on_start(self):
-#         self.client.post("/", json={"username":"", "password":""}) 
-        
-#Sample code 2:
+# class MySecondTest(User):
+#     weight = 2
+#     wait_time = constant(1)
 
-class MyFirstTest(User):
-    weight = 2
-    wait_time = constant(1)
+#     @task
+#     def launch2(self):
+#         print("Second test")
 
-    @task
-    def launch(self):
-        print("Launching the URL")
-
-    @task
-    def search(self):
-        print("Searching")
-
-class MySecondTest(User):
-    weight = 2
-    wait_time = constant(1)
-    
-    @task
-    def launch2(self):
-        print("Second test")
-
-    @task 
-    def search2(self):
-        print("Second search test")
+#     @task 
+#     def search2(self):
+#         print("Second search test")
